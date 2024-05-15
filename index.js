e36b737eb65d8bd737e168b872d8833daca1ddd1
JesiahTapia <siahtapia@gmail.com>
2024-05-15 10:32:08 -0400

    Add database configuration and functionality

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const doxname = process.env.SECRET_USER
 const PORT = process.env.PORT
 console.log(doxname);
 
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
 const express = require('express')
 const app = express()
 
@@ -12,3 +15,37 @@ app.get('/', function (req, res) {
 })
 
 app.listen(PORT)
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+  
+  // Connect to the database
+  connection.connect()
+  
+  // Define SQL query to insert a new user
+  const newUser = { FirstName: 'Jesiah', LastName: 'Tapia' }
+  const query = 'INSERT INTO employees SET ?'
+  
+  // Execute the SQL query
+  connection.query(query, newUser, (error, results) => {
+    if (error) {
+      console.error('Error adding user:', error)
+    } else {
+      console.log('New user added successfully:', results)
+    }
+  })
+  
+  // Close the connection
+  connection.end();
+
+
+const host = process.env.DB_HOST;
+const user = process.env.DB_USER;
+
+console.log(host);
+console.log(user);
+
